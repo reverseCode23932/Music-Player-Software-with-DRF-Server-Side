83dcc781ce4cb16f8e3db106330bcb89eb74b302
reverseCode23932 <gamerxdlike0@gmail.com>
2025-10-16 21:16:52 +0300

    render

--- server/server/settings.py
+++ server/server/settings.py
@@ -28,7 +28,7 @@ assert os.getenv("SECRET_KEY"), "no secret_key provided"
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
